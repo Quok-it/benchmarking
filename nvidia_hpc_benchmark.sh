dceb1913ce85e6523859ed279417b444776d463f
tarunyaa <tarunyaa@sas.upenn.edu>
2025-05-02 02:28:45 -0400

    added hpc, small edits

--- nvidia_hpc_benchmark.sh
+++ nvidia_hpc_benchmark.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd .. 
 wget https://developer.download.nvidia.com/compute/nvidia-hpc-benchmarks/25.04/local_installers/nvidia-hpc-benchmarks-local-repo-ubuntu2204-25.04_1.0-1_amd64.deb
 sudo dpkg -i nvidia-hpc-benchmarks-local-repo-ubuntu2204-25.04_1.0-1_amd64.deb
 sudo cp /var/nvidia-hpc-benchmarks-local-repo-ubuntu2204-25.04/nvidia-hpc-benchmarks-*-keyring.gpg /usr/share/keyrings/
@@ -11,3 +12,4 @@ cd /opt/nvidia/nvidia_hpc_benchmarks_openmpi
 ./hpcg.sh --dat ./hpl-linux-x86_64/sample-dat/HPL-1GPU.dat --nx 256 --ny 256 --nz 256 --rt 2 | tee hpcg_results.txt
 ./stream-gpu-test.sh --d 0 --n 100000000 | tee stream_results.txt
 cd ..
+cd benchmarking
