cc1a20c0c6910b304f51c4bc76a14b5f04d727e8
tarunyaa <tarunyaa@sas.upenn.edu>
2025-05-03 14:17:22 -0400

    full benchmarking script first draft

--- nvidia_hpc_benchmark.sh
+++ nvidia_hpc_benchmark.sh
@@ -5,8 +5,8 @@ wget https://developer.download.nvidia.com/compute/nvidia-hpc-benchmarks/25.04/l
 sudo dpkg -i nvidia-hpc-benchmarks-local-repo-ubuntu2204-25.04_1.0-1_amd64.deb
 sudo cp /var/nvidia-hpc-benchmarks-local-repo-ubuntu2204-25.04/nvidia-hpc-benchmarks-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update
-sudo apt install nvidia-hpc-benchmarks-openmpi
-sudo apt install libopenmpi-dev
+sudo apt install -y nvidia-hpc-benchmarks-openmpi
+sudo apt install -y libopenmpi-dev
 cd /opt/nvidia/nvidia_hpc_benchmarks_openmpi
 ./hpl.sh --dat ./hpl-linux-x86_64/sample-dat/HPL-1GPU.dat | tee ~/benchmarking/hpl_results.txt
 ./hpcg.sh --dat ./hpl-linux-x86_64/sample-dat/HPL-1GPU.dat --nx 256 --ny 256 --nz 256 --rt 2 | tee ~/benchmarking/hpcg_results.txt
