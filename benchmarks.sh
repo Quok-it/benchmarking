bf6680a2fb15e0a620a7824e237cb100ea70f849
Rahul Chandra <rahul@chandra.net>
2025-07-04 01:08:54 -0400

    Remove dangerous system level pip
    I have 0 clue how this even worked? Maybe the error was just never
    seen because the script wasn't exiting on error before

--- benchmarks.sh
+++ benchmarks.sh
@@ -12,9 +12,8 @@ sudo apt-get install -y unzip wget curl
 # GPU Burn Dependencies (pulls gcc, stdc headers, glibc headers, dpkg stuff, and make)
 sudo apt-get install -y build-essential
 
-pip install --upgrade pip
-pip install --no-cache-dir pymongo
-pip install python-dotenv
+# Python module dependencies, never use pip without a venv
+sudo apt-get install -y python3-pymongo python3-dotenv
 
 chmod +x mlperf_benchmark_datacenter.sh
 chmod +x gpu_burn.sh
